9b38ab7de0513a3ca63d8d879b17060ed593ea9e
jiajun 4090 <jhu114514@gmail.com>
2025-09-02 16:27:02 +0200

    init

---
+++ install/install.sh
@@ -0,0 +1,9 @@
+pip install mujoco
+pip install mujoco_mjx     
+pip install brax           # 安装 Google 的 JAX 版可微物理引擎
+pip install playground     # 安装 mujoco playground
+
+pip uninstall jax jaxlib   # 卸载 CPU 版本 Jax
+pip install jax[cuda12]    # 安装 Cuda 版本 Jax
+
+pip install mediapy        # 安装 mediapy

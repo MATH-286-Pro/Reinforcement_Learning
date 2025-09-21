bdc66d809246cb44b8ae166512ed2e9e0d450075
ECE-486-Pro <jhu114514@gmail.com>
2025-09-21 16:53:59 +0200

    双环 PID (速度环 + 直立环)

--- MY_TEST/project1/controller/PID.py
+++ MY_TEST/project1/controller/PID.py
@@ -10,6 +10,9 @@ import env.cartpole_env as cartpole_env
 def main():
     env = cartpole_env.SimplePendulumEnv(render_mode="human")
 
+    # 模型信息
+    pole_length = 0.6
+
     obs, _ = env.reset()
     terminated = False
 
@@ -17,10 +20,28 @@ def main():
         x, x_dot, cos_theta, sin_theta, theta_dot = obs
         theta = np.arctan2(sin_theta, cos_theta)
 
+        # 速度环 PID 控制
+        VELOCITY_KP = 0.6
+        VELOCITY_KD = 1.0   # 这里因为没有 acc 的信息所以条这个参数没用
+        target_vel = 0
+        current_vel = x_dot + pole_length * theta_dot  # 考虑小车速度和摆杆角速度的组合
+
+        error_vel     =  target_vel - current_vel  # 目标速度
+        error_vel_dot = 0                          # 目标加速度为0
+
+        vel_loop_output = error_vel * VELOCITY_KP + error_vel_dot * VELOCITY_KD
+
+
         # 直立环 PID 控制
-        KP = 10.0
-        KD = 0.1
-        action = theta * KP + theta_dot * KD  # PID 控制器
+        ANG_KP = -10.0
+        ANG_KD = -0.1
+        target_angle  = vel_loop_output
+        current_angle = theta
+
+        error_angle     = target_angle - current_angle
+        error_angle_dot = - theta_dot
+
+        action = error_angle * ANG_KP + error_angle_dot * ANG_KD  # PID 控制器
 
         # 执行动作
         obs, _ , terminated, truncated, _ = env.step([action])
